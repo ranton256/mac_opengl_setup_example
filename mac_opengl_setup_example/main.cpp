4d3a6029a122cc5e96216bc924dbe1d5b33f108a
Richard Anton <ranton@ranton.org>
2023-01-07 12:11:24 -0800

    Initial Commit

---
+++ mac_opengl_setup_example/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mac_opengl_setup_example
+//
+//  Created by Richard Anton on 1/7/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
